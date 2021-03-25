24e49384ca86171b2f51a71d593cc77270e527d8
ibrahimGuilherme <iibrahimguilherme00@gmail.com>
2021-03-25 16:27:30 -0300

    aulas

---
+++ aula39_DicionariosExemplosDeUso.py
@@ -0,0 +1,54 @@
+# d1 = {}
+# print(d1)
+# print(type(d1))
+#
+# d1['a'] = 1
+# d1['b'] = 2
+#
+# print(d1)
+
+#d2 = dict()
+
+# d2 = dict(c='carro', b='bicicleta')
+
+#print(d2)
+# print(type(d2))
+#print(len(d2))
+#print(d2['a'])
+
+# valor = d2.get('b')
+# if valor:
+#     print(f'a chave b está no dicionário e vale {valor}')
+# else:
+#     print('a chave não está no dicionário')
+#
+# valor = d2.get('a')
+# if valor:
+#     print(f'a chave a está no dicionário e vale {valor}')
+# else:
+#     print('a chave a não está no dicionário')
+
+d3 = dict(c='carro', b='bicicleta')
+#
+# d4 = d3
+#
+# print(d3)
+# print(d4)
+#
+# d3['c'] = 'caminhão'
+# print(d3)
+# print(d4)
+
+
+# d4 = d3.copy()
+# print(d3)
+# print(d4)
+# d3['c'] = 'caminhão'
+# print(d3)
+# print(d4)
+
+print(d3)
+# del(d3['c'])
+d3.clear()
+
+print(d3)
