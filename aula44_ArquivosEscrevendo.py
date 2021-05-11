b04b372ae2f67e147bbe4278534a20056438d995
ibrahimGuilherme <iibrahimguilherme00@gmail.com>
2021-05-11 19:56:27 -0300

    aulas

--- aula44_ArquivosEscrevendo.py
+++ aula44_ArquivosEscrevendo.py
@@ -1,3 +1,19 @@
-arq = open('teste.txt', 'w')
+# arq = open('teste.txt', 'a')
+# arq.write('Curso de Python\n')
+# arq.write('Curso de SQL\n')
+# arq.close()
 
-arq.close()
+arq1 = open('amigos.txt', 'r')
+arq2 = open('teste.txt', 'w')
+
+while True:
+    texto = arq1.readline()
+
+    if texto == '':
+        break
+
+    if texto[:5] == 'ffffj':
+        continue
+    arq2.write(texto)
+arq1.close()
+arq2.close()
